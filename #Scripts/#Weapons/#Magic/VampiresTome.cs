7de3af5e78e75c483b1dded733b536b15b696ac1
Markus Piotrowski <70880588+Mackapacka15@users.noreply.github.com>
2020-11-12 19:24:55 +0100

    VampiresTome done

---
+++ #Scripts/#Weapons/#Magic/VampiresTome.cs
@@ -0,0 +1,62 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using OratiumMod.Items.Projectiles;
+using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OratiumMod.Items.Weapons.Magic
+{
+	public class VampiresTome : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Vampires Tome");
+			Item.staff[item.type] = true; //this makes the useStyle animate as a staff instead of as a gun
+		}
+
+		public override void SetDefaults() {
+			item.damage = 120;
+			item.magic = true;
+			item.mana = 2;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 8;
+			item.useAnimation = 8;
+			item.useStyle = 5;
+			item.noMelee = true; //so the item's animation doesn't do damage
+			item.knockBack = 5;
+			item.value = 10000;
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item20;
+			item.autoReuse = true;
+			item.shootSpeed = 20f;
+            item.shoot=ProjectileID.VampireKnife;
+		}
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            type = ProjectileID.VampireKnife;
+
+            int numberProjectiles = 5;
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(10));
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }
+            return false;
+        }
+        
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("AdamantiumBar"), 12);
+            recipe.AddIngredient(mod.ItemType("SoulofIgnite"), 4);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+		
+	}
+}
