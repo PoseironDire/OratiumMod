71450bcf165971b9a0a3e994f3ecdf693648fb41
Markus Piotrowski <70880588+Mackapacka15@users.noreply.github.com>
2020-11-12 18:36:26 +0100

    .

--- #Scripts/#Weapons/#Bows/BlisterBow.cs
+++ #Scripts/#Weapons/#Bows/BlisterBow.cs
@@ -23,7 +23,7 @@ namespace OratiumMod.Items.Weapons.Bows
             item.useStyle = ItemUseStyleID.HoldingOut; // how you use the item (swinging, holding out, etc)
             item.noMelee = true; //so the item's animation doesn't do damage
             item.knockBack = 8; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
-            item.value = (silver:100000); // how much the item sells for (measured in copper)
+            item.value =  100000; // how much the item sells for (measured in copper)
             item.rare = 4; // the color that the item's name will be in-game
             item.UseSound = SoundID.Item5; // The sound that this item plays when used.
             item.autoReuse = false; // if you can hold click to automatically use it again
